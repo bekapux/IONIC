e02bdb97c948f4ab3b24f8c3d364a9a1feb961c1
DESKTOP-VLB7TDB\Beka <beka.pukhashvili@gmail.com>
2023-03-20 20:09:12 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jondi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
